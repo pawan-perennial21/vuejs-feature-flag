f8059121d9e2e2b6589a97536313ad618d1726e9
pawan-perennial21 <pawan.patidar@perennialsys.com>
2024-10-23 09:20:31 +0530

    flag feature

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,120 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div id="app">
+		<button class='btn' @click='getUserAgeFeatureStatus'>
+			Calculate Age
+		</button>
+		
+		<p v-if='!userAgeFeature && !loading' class='text'>Sorry, This feature has been disabled by the Admin</p>
+		<p v-else-if="error">{error}</p>
+
+		<div v-else-if='userAgeFeature'>
+			<p class='text'>Calculate your age below by providing your year of birth</p>
+			<input
+				type='number'
+				v-model='birthYear'
+			/>
+			<button class='btn btn-calculate' @click='calcAge'>
+				Calculate
+			</button>
+
+			<p v-if='age'>You are {{ age }} years old</p>
+		</div>
+	</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import * as configcat from "configcat-js";
+
+export default {
+	name: 'App',
+	data() {
+		return {
+			userAgeFeature: null,
+			error: null,
+			birthYear: '',
+			age: '',
+			loading: true
+		};
+	},
+	methods: {
+		// Get feature status from ConfigCat
+		async getUserAgeFeatureStatus() {
+      console.log("clicked")
+			try {
+				const configCatClient = configcat.getClient(
+					"configcat-sdk-1/kfLcCLn2TU6C2QE4krHiew/4hglqXmsWk2vYS5GC6-M4w"
+				);
+				console.log({configCatClient})
+				// Retrieve the feature flag value
+				const res = await configCatClient.getValueAsync("calculateuseragefeature", false);
+        console.log(res)
+				this.userAgeFeature = res;
+			} catch (err) {
+				this.error = err.message;
+			}
+			this.loading = false;
+		},
+		// Calculate age feature
+		calcAge() {
+			const currentYear = new Date().getFullYear();
+			this.age = currentYear - this.birthYear;
+			this.birthYear = '';
+		}
+	}
+};
+</script>
+
+<style>
+body {
+	margin: 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+#app {
+	text-align: center;
+	 background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.btn {
+	color: rgb(16, 82, 54);
+	  background-color: royalblue;
+	border: none;
+	 padding: 8px 16px;
+	border-radius: 15px;
+	font-size: 1.2rem;
+	cursor: pointer;
+	font-weight: 600;
+}
+
+.btn:hover {
+	background-color: rgb(16, 82, 54);
+	color: #FFF;
+}
+
+.btn:focus-visible,
+input:focus-visible {
+	outline: none;
+}
+
+.btn-calculate {
+	padding: 10px 25px;
+	margin-left: 8px;
+	border-radius: 8px;
+}
+
+input {
+	padding: 10px 25px;
+	font-size: 1.2rem;
+	border-radius: 8px;
+	border: none;
+}
+
+.text {
+	font-size: 1.5em;
+	color: #FFF;
 }
 </style>
